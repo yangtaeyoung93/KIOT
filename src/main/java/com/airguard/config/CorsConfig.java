eb158ebe0260d12767aa0e49b829c4ddabc14a6e
taeyoungYang93 <ngj100@naver.com>
2022-06-04 18:57:56 +0900

    필터알림 시 알림설정값 조회 수정, cors 수정

--- src/main/java/com/airguard/config/CorsConfig.java
+++ src/main/java/com/airguard/config/CorsConfig.java
@@ -26,9 +26,18 @@ public class CorsConfig implements WebMvcConfigurer {
         .allowedMethods("*").allowCredentials(true);
 
     registry.addMapping("/api/collection/history")
-            .allowedOrigins("http://localhost:8889","http://220.95.232.79:8889","http://issi.kweather.co.kr","http://issi.kweather.co.kr:8889") //인천 서구 프로젝트의 데이터 수집용
+            .allowedOrigins("http://localhost:8889",
+                    "http://220.95.232.79:8889",
+                    "http://issi.kweather.co.kr",
+                    "http://issi.kweather.co.kr:8889",
+                    "http://211.219.114.113:8889") //인천 서구 프로젝트의 데이터 수집용
             .allowedMethods("*")
             .allowCredentials(true);
+
+    registry.addMapping("/system/member/device/ajax/fileDownload/**")
+            .allowedOrigins("http://localhost")
+            .allowedOrigins("*")
+            .allowedMethods("*").allowCredentials(true);
 
     registry.addMapping("/api/air365/v3/data/detail")
             .allowedOrigins("http://localhost")
