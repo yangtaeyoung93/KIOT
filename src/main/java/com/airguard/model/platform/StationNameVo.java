bfb82abe549588390a7297c5a4b91d3df6977791
taeyoungYang93 <ngj100@naver.com>
2022-07-28 09:10:40 +0900

    에어맵 시리얼번호,스테이션명 제공 API

---
+++ src/main/java/com/airguard/model/platform/StationNameVo.java
@@ -0,0 +1,28 @@
+/**
+ * @FileName : ResultSensorVo.java
+ * @Project : KIOT_ADMIN
+ * @Date : 2020. 4. 22.
+ * @Auth : Kim, DongGi
+ */
+package com.airguard.model.platform;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+/**
+ * @FileName : AirMapOaqVo.java
+ * @Project : KIOT
+ * @Date : 2021. 3. 11.
+ * @Auth : Yoo, HS
+ */
+@Getter
+@Setter
+@ToString
+public class StationNameVo {
+
+  private String serial;
+  private String stationName;
+}
